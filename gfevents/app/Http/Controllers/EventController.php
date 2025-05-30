c692b163f496e6f614a432dd112a65edd17c323d
Guilherme Stein <guilhermezuninosgrott@gmail.com>
2025-05-29 21:12:26 -0300

    Views

--- gfevents/app/Http/Controllers/EventController.php
+++ gfevents/app/Http/Controllers/EventController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event; 
+
 class EventController extends Controller
 {
     public function index()
     {
-        // Logic to retrieve and display events
-        return view('welcome');
+        $events = Event::all();
+
+        return view('welcome',['events' => $events]);
+
     }
     public function create(){
         return view('events.create');
--- gfevents/resources/views/welcome.blade.php
+++ gfevents/resources/views/welcome.blade.php
@@ -3,8 +3,29 @@
 @section('title', 'GFevents')
 
 @section('content')
-        <h1>Teste</h1>
-        <img src="/img/banner.jpg" alt="">
 
-@endsection        
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="/img/evento.jpg" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">10/09/2020</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="#" class="btn btn-primary">Saiba Mais</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 
+        @endsection
